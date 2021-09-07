9e1f2f10424d7847a91a01daa77e2c7f752221ef
Dvam leite <dvanlds@gmail.com>
2021-09-07 19:35:12 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Link from 'next/link'
 
 import Menu from '../components/menu'
+import Pokemon from '../pages/pokemon/[id]'
 
 
 export async function getStaticProps(context){
@@ -26,20 +27,21 @@ export async function getStaticProps(context){
  export default function App(props){
 
     const { pokemons } = props;
-console.log(pokemons)
+    console.log(pokemons)
+
      return(
         <>
         <Menu />
           <div className="content">
                  <h1>Pokédex Com Nextjs</h1>
 
-                
                     <div className="cardcont">
                         {pokemons.map((pokemon) => (
                             <div key={pokemon.entry_number}>
                             <Link href={`/pokemon/${pokemon.entry_number}`}>
 
-                               <div className="card">                               
+                               <div className="card">                                 
+                               <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.entry_number}.png`} />
                                     <a>
                                         {pokemon.pokemon_species.name}
                                     </a>
@@ -47,8 +49,7 @@ console.log(pokemons)
                             </Link>
                             </div>
                         ))}
-                    </div>
-                
+                    </div>                
           </div>
         </>
      )
--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -7,17 +7,34 @@ export default function Pokemon({pokemon}){
         <>  
             <Menu />         
             <div className="details">
-                <img src={pokemon.sprites.front_default} alt="Imagem de um Pokemon" />
-                <ul>
-                    <li>
+
+               <div className="detailsCard">
+                    <img src={pokemon.sprites.front_default} alt="Imagem de um Pokemon" />
                         <h2>{pokemon.name}</h2>
-                        <h2>XP: {pokemon.base_experience}</h2>                    
-                    </li>
-                </ul>
+
+                        <div className="stats">
+                                            
+                            <h2>HP: {pokemon.stats[0].base_stat}</h2>
+                            <h2>STR: {pokemon.stats[1].base_stat}</h2>
+                            <h2>DEF: {pokemon.stats[2].base_stat}</h2>
+                            <h2>SPEED: {pokemon.stats[3].base_stat}</h2>
+
+                        </div>
+
+                        <h2>XP: {pokemon.base_experience}</h2>
+
+                        <div className="infos">
+                            <h2>ID: {pokemon.id}</h2>
+                            <h2>Tamanho: {pokemon.weight}</h2>
+                            <h2>Ordem: {pokemon.order}</h2>
+                        </div>
+               </div>                  
+  
             </div>
         </>
     );
 }
+
 
 export async function getStaticProps({ params }){
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
