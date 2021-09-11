3dc46f871e16e1812851425d2503482d4409f061
Dvam leite <dvanlds@gmail.com>
2021-09-10 21:26:25 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,51 @@
-import React from "react"
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-import Menu from '../components/menu'
-import Pokemon from '../pages/pokemon/[id]'
+import Menu from "../components/menu";
 
+export async function getStaticProps(context) {
+  const pokemons = await fetch("https://pokeapi.co/api/v2/pokedex/2/")
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((resObj) => {
+      return resObj.pokemon_entries;
+    });
 
-export async function getStaticProps(context){
-
-  const pokemons = await fetch('https://pokeapi.co/api/v2/pokedex/2/')       
-    .then((res) =>{
-            if(res.ok){
-                return res.json();
-            }
-        })
-    .then((resObj) => {            
-          return  resObj.pokemon_entries;
-        })
-
-        return{
-            props: {
-                pokemons
-            },
-        }
+  return {
+    props: {
+      pokemons,
+    },
+  };
 }
 
- export default function App(props){
+export default function App(props) {
+  const { pokemons } = props;
 
-    const { pokemons } = props;
-    console.log(pokemons)
+  return (
+    <>
+      <Menu />
+      <div className="content">
+        <h1>Pokédex Com Nextjs</h1>
 
-     return(
-        <>
-        <Menu />
-          <div className="content">
-                 <h1>Pokédex Com Nextjs</h1>
-
-                    <div className="cardcont">
-                        {pokemons.map((pokemon) => (
-                            <div key={pokemon.entry_number}>
-                            <Link href={`/pokemon/${pokemon.entry_number}`}>
-
-                               <div className="card"> 
-                               <span className="numb">#{pokemon.entry_number}</span>                                
-                               <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.entry_number}.png`} />
-                                    <a>
-                                        {pokemon.pokemon_species.name}
-                                    </a>
-                               </div>
-                            </Link>
-                            </div>
-                        ))}
-                    </div>                
-          </div>
-        </>
-     )
- }
-
- 
+        <div className="cardcont">
+          {pokemons.map((pokemon) => (
+            <div key={pokemon.entry_number}>
+              <Link href={`/pokemon/${pokemon.entry_number}`}>
+                <div className="card">
+                  <span className="numb">#{pokemon.entry_number}</span>
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.entry_number}.png`}
+                  />
+                  <a>{pokemon.pokemon_species.name}</a>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -1,74 +1,82 @@
-import React from 'react'
-import Menu from '../../components/menu'
+import React from "react";
+import Menu from "../../components/menu";
 
-export default function Pokemon({pokemon}){
-    console.log(pokemon)    
-    return(
-        <>  
-            <Menu />         
-            <div className="details">
+export default function Pokemon({ pokemon }) {
+  console.log(pokemon);
+  return (
+    <>
+      <Menu />
+      <div className="details">
+        <div className="detailsCard">
+          <img src={pokemon.sprites.front_default} alt="Imagem de um Pokemon" />
+          <h3>{pokemon.name}</h3>
 
-               <div className="detailsCard">
-                    <img src={pokemon.sprites.front_default} alt="Imagem de um Pokemon" />
-                        <h2>{pokemon.name}</h2>
-
-                        <div className="stats">
-                                            
-                            <h2>HP: {pokemon.stats[0].base_stat}</h2>
-                            <h2>STR: {pokemon.stats[1].base_stat}</h2>
-                            <h2>DEF: {pokemon.stats[2].base_stat}</h2>
-                            <h2>SPEED: {pokemon.stats[3].base_stat}</h2>
-
-                        </div>
-
-                        <h2>XP: {pokemon.base_experience}</h2>
-
-                        <div className="infos">
-                            <h2>ID: {pokemon.id}</h2>
-                            <h2>Ordem: {pokemon.order}</h2>
-                            <h2>Tamanho: {pokemon.weight}</h2>
-                           
-                        </div>
-               </div>                  
-  
+          <div className="type">
+            <div className="typ">
+              <h4>Tipo: {pokemon.types[0].type.name}</h4>
             </div>
-        </>
-    );
+          </div>
+
+          <div className="stats">
+            <div className="barstatsHP hp">
+              <h2>HP: {pokemon.stats[0].base_stat}</h2>
+            </div>
+            <div className="barstatsSTR hp">
+              <h2>STR: {pokemon.stats[1].base_stat}</h2>
+            </div>
+            <div className="barstatDEF hp">
+              <h2>DEF: {pokemon.stats[2].base_stat}</h2>
+            </div>
+            <div className="barstatsSPEED hp">
+              <h2>SPEED: {pokemon.stats[3].base_stat}</h2>
+            </div>
+          </div>
+
+          <div className="bar">
+            {" "}
+            <h2 className="barstats">XP: {pokemon.base_experience}</h2>{" "}
+          </div>
+
+          <div className="infos">
+            <h2>Tamanho: {pokemon.weight}</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
+export async function getStaticProps({ params }) {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
 
-export async function getStaticProps({ params }){
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
-    .then((res) =>{
-        if(res.ok){
-            return res.json();
-        }
-
-        throw new Error('Deu Erro')
+      throw new Error("Deu Erro");
     })
-    .then((resobj) => resobj)
-    return{
-        props: {
-            pokemon
-        }
-    }
+    .then((resobj) => resobj);
+  return {
+    props: {
+      pokemon,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-
-    const pokemons = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+export async function getStaticPaths() {
+  const pokemons = await fetch("https://pokeapi.co/api/v2/pokedex/2/")
     .then((respostaDoServer) => {
       if (respostaDoServer.ok) {
         return respostaDoServer.json();
       }
-      throw new Error('Deu problema');
+      throw new Error("Deu problema");
     })
     .then((respostaEmObjeto) => respostaEmObjeto.pokemon_entries);
 
-    return{      
-        paths: pokemons.map((pokemon) => ({ //pra cada pokemon na resposta, um caminho sera gerado
+  return {
+    paths: pokemons.map((pokemon) => ({
       params: {
-        id: pokemon.entry_number.toString(), //necessario usar o toString porque não aceita inteiro como caminho (path)
+        id: pokemon.entry_number.toString(),
       },
     })),
     fallback: false,
